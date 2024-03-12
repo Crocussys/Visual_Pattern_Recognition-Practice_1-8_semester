9507d7210de33bb7ddbbc0a90a70b9eee264acf1
Crocussys <egor.vladislavovich.nn@gmail.com>
2024-03-12 12:28:51 +0300

    background not fill

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <string>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/types.hpp>
 
 using namespace cv;
 int main()
@@ -11,16 +11,32 @@ int main()
         std::cout << "Изображение не загружено" << std::endl;
         return -1;
     }
-    MatSize s = img.size;
-    std::string text = "Size: " + std::to_string(s.p[0]) + " x " + std::to_string(s.p[1]);
-    putText(img, text, Point(0, 30), 0, 1.0, Scalar(255, 255, 255), 2);
-    line(img, Point(200, 200), Point(400, 200), Scalar(0, 255, 0), 4);
-    rectangle(img, Point(200, 1000), Point(400, 1200), Scalar(255, 25, 255), -1);
-    rectangle(img, Point(1000, 1000), Point(1200, 1200), Scalar(0, 0, 255), 6);
-    circle(img, Point(800, 800), 100, Scalar(0, 0, 255), 4);
-    circle(img, Point(1200, 200), 100, Scalar(0, 125, 0), -1);
-    namedWindow("Display window", WINDOW_NORMAL);
-    imshow("Display window", img);
+
+    Mat part1 = img(Rect(0, 0, img.cols / 2, img.rows / 2));
+    Mat part2 = img(Rect(img.cols / 2, 0, img.cols / 2, img.rows / 2));
+    Mat part3 = img(Rect(0, img.rows / 2, img.cols / 2, img.rows / 2));
+    Mat part4 = img(Rect(img.cols / 2, img.rows / 2, img.cols / 2, img.rows / 2));
+    imshow("Display window1", part1);
+    imshow("Display window2", part2);
+    imshow("Display window3", part3);
+    imshow("Display window4", part4);
+
+    Mat mask = Mat::zeros(img.size(), img.type());
+    circle(mask, Point(img.cols / 2, img.rows / 2), 200, Scalar(255, 255, 255), -1);
+    imshow("Mask", mask);
+    Mat r = Mat::zeros(img.size(), img.type());
+
+    // std::cout << img.size() << " " << r.rows << " " << r.cols << std::endl;
+
+    bitwise_and(img, mask, r);
+    for(int i = 0; i < r.rows; i++){
+        for(int j = 0; j < r.cols; j++){
+            if(!mask.at<uchar>(i, j)){
+                r.at<uchar>(i, j) = 255;
+            }
+        }
+    }
+    imshow("Result", r);
     waitKey(0);
     return 0;
 }
